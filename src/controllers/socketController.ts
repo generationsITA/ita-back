e9f2949042ad3ad8cc9eebf05b33a4e749c0639c
valeriygl <glyancevval@gmail.com>
2020-05-14 21:21:38 +0300

    added chat interface

--- src/controllers/socketController.ts
+++ src/controllers/socketController.ts
@@ -5,26 +5,35 @@ interface RequestMessage {
   text: string;
 }
 
+interface chatAuth {
+  name: string;
+  room: string;
+}
+
 const socketController = (socket: SocketIO.Socket) => {
-  socket.on('join', ({ name, room }, callback) => {
-    const error = userHandler.addUser(socket.id, name, room);
+  socket.on(
+    'join',
+    ({ name, room }: chatAuth, callback: (error?: object) => void) => {
+      const error = userHandler.addUser(socket.id, name, room);
 
-    if (error) {
-      return callback({ error });
+      if (error) {
+        return callback({ error });
+      }
+      console.log(name);
+      socket.join(room);
+
+      callback();
     }
-    console.log(name);
-    socket.join(room);
+  );
 
-    callback();
-  });
-
-  socket.on('sendMessage', ({ text }: RequestMessage) => {
+  socket.on('sendMessage', ({ text }: RequestMessage, callback: () => void) => {
     // const address =
     // socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
-    const { name } = userHandler.getUser(socket.id);
+    const { name, room } = userHandler.getUser(socket.id);
 
-    socket.broadcast.to('general').emit('message', { name, text });
+    socket.broadcast.to(room).emit('message', { name, text });
     console.log(`${name}: ${text}`);
+    callback();
   });
 
   socket.on('disconnect', () => {
